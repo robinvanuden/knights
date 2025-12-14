db0087e8a44ce70b66f9459192a56dc458a9d48b
Robin <robinvanuden@gmail.com>
2025-12-14 12:10:29 +0100

    Added toggle for free movement in PlayerMovement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour {
     private Animator _animator;
     private Vector2 _movement;
     private Rigidbody2D _rb;
+    
+    public bool freeMovement = false;
 
     private void Awake() {
         _rb = GetComponent<Rigidbody2D>();
@@ -29,21 +31,23 @@ public class PlayerMovement : MonoBehaviour {
 
     private void OnMovement(InputValue value) {
         // TODO: Decide to remove or add free movement
-        // _movement = value.Get<Vector2>();
-        
-        // 1-directional movement (no diagonals): prefer the axis with the larger magnitude.
-        var movement = value.Get<Vector2>();
-        // If both inputs are near zero, stop completely
-        if (Mathf.Approximately(movement.x, 0f) && Mathf.Approximately(movement.y, 0f)) {
-            _movement = Vector2.zero;
-            return;
-        }
-
-        // Choose dominant axis to avoid diagonal movement
-        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y)) {
-            _movement = new Vector2(Mathf.Sign(movement.x), 0f);
+        if (freeMovement){
+            _movement = value.Get<Vector2>();
         } else {
-            _movement = new Vector2(0f, Mathf.Sign(movement.y));
+            // 1-directional movement (no diagonals): prefer the axis with the larger magnitude.
+            var movement = value.Get<Vector2>();
+            // If both inputs are near zero, stop completely
+            if (Mathf.Approximately(movement.x, 0f) && Mathf.Approximately(movement.y, 0f)) {
+                _movement = Vector2.zero;
+                return;
+            }
+
+            // Choose dominant axis to avoid diagonal movement
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y)) {
+                _movement = new Vector2(Mathf.Sign(movement.x), 0f);
+            } else {
+                _movement = new Vector2(0f, Mathf.Sign(movement.y));
+            }
         }
     }
 

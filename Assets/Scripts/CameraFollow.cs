85d8eaae0263e01781053f56cf0a15125d867f72
Robin <robinvanuden@gmail.com>
2025-12-14 10:33:03 +0100

    Added camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public float smoothTime = 0.25f;
+
+    // Offset from the target (Vector3.zero will not work)
+    private readonly Vector3 _offset = new(0f, 0f, -10f);
+    private Vector3 _velocity = Vector3.zero;
+
+    [SerializeField] private Transform target;
+
+    private void Update() {
+        // Skip if target is null
+        if (!target) return;
+        // Smoothly move towards the target
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            target.position + _offset,
+            ref _velocity,
+            smoothTime
+        );
+    }
+}
